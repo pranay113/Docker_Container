53061ab61791c4c640ab5721a0b43ec305073588
pranay chinche <78250986+pranay113@users.noreply.github.com>
2021-06-25 23:23:58 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,17 @@
+#!  /usr/bin/python3
+
+import cgi
+import subprocess
+
+
+print()
+
+mydata  = cgi.FieldStorage()
+
+info  = mydata.getvalue("info")
+
+
+cmd = subprocess.getoutput("sudo "+ info)
+
+
+print(cmd)
